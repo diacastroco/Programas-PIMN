9e47efb4ff169550acb4181c195b0e40d0ea4444
DiegoCastro <diacastroco@unal.edu.co>
2017-09-12 18:00:17 -0700

    Metodo secante para raices

---
+++ secante.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+double f(double x);
+
+int main(void)
+{
+  std::cout.precision(16);
+  std::cout.setf(std::ios::scientific);
+  int NMAX=50;
+  double xr=0.0, xi1=-2.0, xi=-1.0, eps=1.0e-10;
+
+  for (int ii=1; ii<=NMAX; ii++)
+    {
+      xr=xi-(f(xi)*(xi1-xi))/(f(xi1)-f(xi));
+      xi=xi1;
+      xi1=xr;
+      std::cout << xi << '\t'
+                << xi1 << '\t'
+                << xr << '\t'
+                <<f(xr) << '\t'
+		<< std::fabs(1-std::fabs(xi/xi1))*100 << std::endl;
+      if(std::fabs(f(xr)<=eps))
+	{
+	  break;
+	}
+    }   
+  std::cout << xr << std::endl;
+  return 0;
+}
+
+
+double f(double x)
+{
+  return (x*x)-5*x+1;
+}
